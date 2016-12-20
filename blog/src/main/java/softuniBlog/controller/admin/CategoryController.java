8f6f90b0c503c982ba4e9f1c196aa244fb0893a9
TNinov <filo53@abv.bg>
2016-12-20 18:01:34 +0200

    add

--- blog/src/main/java/softuniBlog/controller/admin/CategoryController.java
+++ blog/src/main/java/softuniBlog/controller/admin/CategoryController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import softuniBlog.bindingModel.CategoryBindingModel;
+import softuniBlog.entity.Article;
 import softuniBlog.entity.Category;
+import softuniBlog.repository.ArticleRepository;
 import softuniBlog.repository.CategoryRepository;
 import java.util.List;
 
@@ -20,6 +22,9 @@ public class CategoryController {
 
     @Autowired
     private CategoryRepository categoryRepository;
+
+    @Autowired
+    private ArticleRepository articleRepository;
 
     @GetMapping("/")
     public String list (Model model){
@@ -68,5 +73,50 @@ public class CategoryController {
 
         return "base-layout";
 
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editProcess(@PathVariable Integer id, CategoryBindingModel categoryBindingModel){
+        if (!this.categoryRepository.exists(id)){
+            return "redirect:/admin/categories/";
+        }
+
+        Category category = this.categoryRepository.findOne(id);
+
+        category.setName(categoryBindingModel.getName());
+
+        this.categoryRepository.saveAndFlush(category);
+
+        return "redirect:/admin/categories/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(Model model, @PathVariable Integer id){
+        if (!this.categoryRepository.exists(id)){
+            return "redirect:/admin/categories/";
+        }
+
+        Category category = this.categoryRepository.findOne(id);
+
+        model.addAttribute("category", category);
+        model.addAttribute("view", "admin/category/delete");
+
+        return "base-layout";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteProcess(@PathVariable Integer id){
+        if (!this.categoryRepository.exists(id)){
+            return "redirect:/admin/categories/";
+        }
+
+        Category category = this.categoryRepository.findOne(id);
+        for (Article article : category.getArticles()){
+            this.articleRepository.delete(article);
+        }
+
+        this.categoryRepository.delete(category);
+
+        return "redirect:/admin/categories/";
     }
 }
